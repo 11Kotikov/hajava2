ca0e271890798c19ed8289337fe98cd5221f19c1
Kotikov <11deadsluts@gmail.com>
2023-02-20 03:01:51 +0300

    kaibun it's palindrome in Japanese :)

---
+++ kaibun.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class kaibun {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type smth: ");
+        String smth = input.nextLine();
+        System.out.println(smth); 
+        input.close();
+    }
+    public static void palindrome () {
+            
+    }
+}
